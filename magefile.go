c81bcfaefbba693a71b70c0670048e6fc7635411
Nate Finch <nate.finch@gmail.com>
2017-10-27 13:00:30 -0400

    make the bin directory as needed, just like go install

--- magefile.go
+++ magefile.go
@@ -30,13 +30,19 @@ func Install() error {
 		return fmt.Errorf("can't determine GOPATH: %v", err)
 	}
 	paths := strings.Split(gopath, string([]rune{os.PathListSeparator}))
-	bin := filepath.Join(paths[0], "bin", name)
+	bin := filepath.Join(paths[0], "bin")
+	// specifically don't mkdirall, if you have an invalid gopath in the first
+	// place, that's not on us to fix.
+	if err := os.Mkdir(bin, 0700); err != nil && !os.IsExist(err) {
+		return fmt.Errorf("failed to create %q: %v", bin, err)
+	}
+	path := filepath.Join(bin, name)
 
 	// we use go build here because if someone built with go get, then `go
 	// install` turns into a no-op, and `go install -a` fails on people's
 	// machines that have go installed in a non-writeable directory (such as
 	// normal OS installs in /usr/bin)
-	return sh.RunV("go", "build", "-o", bin, "-ldflags="+ldf, "github.com/magefile/mage")
+	return sh.RunV("go", "build", "-o", path, "-ldflags="+ldf, "github.com/magefile/mage")
 }
 
 // Generates a new release.  Expects the TAG environment variable to be set,
