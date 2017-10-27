7249b24bb887489a1b8316e9ebd454fa8692888d
Nate Finch <nate.finch@gmail.com>
2017-10-27 14:05:37 -0400

    add CI test for bootstrap

---
+++ install_test.go
@@ -0,0 +1,31 @@
+//+build CI
+
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
+
+func TestBootstrap(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	s, err := run("go", "run", "bootstrap.go")
+	if err != nil {
+		t.Fatal(s)
+	}
+	name := "mage"
+	if runtime.GOOS == "windows" {
+		name += ".exe"
+	}
+	if _, err := os.Stat(filepath.Join(os.Getenv("GOPATH"), "bin", name)); err != nil {
+		t.Fatal(err)
+	}
+}
