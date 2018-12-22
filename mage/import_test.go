d16d0cb083312f06c256495a08d832982123d911
Andrew Kroh <andrew.kroh@elastic.co>
2018-12-22 00:54:11 -0500

    Sort target list
    When using the mage:import feature the target list (`mage -l`) will now be sorted.

--- mage/import_test.go
+++ mage/import_test.go
@@ -22,11 +22,11 @@ func TestMageImportsList(t *testing.T) {
 	actual := stdout.String()
 	expected := `
 Targets:
-  root               
-  zz:nS:deploy2*     deploys stuff.
-  zz:buildSubdir2    Builds stuff.
-  nS:deploy          deploys stuff.
   buildSubdir        Builds stuff.
+  nS:deploy          deploys stuff.
+  root               
+  zz:buildSubdir2    Builds stuff.
+  zz:nS:deploy2*     deploys stuff.
 
 * default target
 `[1:]
