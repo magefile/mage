71947488cf2e68126ff823bdc9f7325432e673f2
Nate Finch <nate.finch@gmail.com>
2020-08-12 16:59:58 -0400

    Update sh/cmd.go

--- sh/cmd.go
+++ sh/cmd.go
@@ -135,7 +135,7 @@ func run(env map[string]string, stdout, stderr io.Writer, cmd string, args ...st
 
 	var quoted []string 
 	for i := range args {
-		quoted=append(quoted,fmt.Sprintf("%q",args[i]));
+		quoted = append(quoted, fmt.Sprintf("%q", args[i]));
 	}
 	log.Println("exec:", cmd,  strings.Join(quoted, " "))
 	err = c.Run()
