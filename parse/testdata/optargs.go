d0d774fc11872a83276f42630b197f09daa6d0fb
Nate Finch <natefinch@github.com>
2026-03-07 18:49:57 -0500

    add files

---
+++ parse/testdata/optargs.go
@@ -0,0 +1,18 @@
+//go:build mage
+// +build mage
+
+package main
+
+import "time"
+
+func OptionalString(name string, greeting *string) {}
+
+func OptionalInt(a int, b *int) {}
+
+func OptionalFloat64(value float64, factor *float64) {}
+
+func OptionalBool(verbose *bool) {}
+
+func OptionalDuration(base time.Duration, extra *time.Duration) {}
+
+func AllOptional(a *string, b *int) {}
